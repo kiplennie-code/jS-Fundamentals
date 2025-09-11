15a36c0ec2f7c5faf83a0b9148f3edb260aa31a2
kipkoech leonard <kiplennie@gmail.com>
2025-09-11 15:18:34 +0300

    Add script that prints 3 lines using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of strings
+let languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
